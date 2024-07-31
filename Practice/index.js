32baa17207000407963d6b22b007e33f95790b26
Bhoomi Gohil <bhoomi_gohil@hotmail.com>
2024-07-31 18:19:37 -0400

    Update code

--- Practice/index.js
+++ Practice/index.js
@@ -1,1 +1,14 @@
 "use strict";
+const message = "Hello World!";
+// Accessing the property 'toLowerCase'
+// on 'message' and then calling it
+message.toLowerCase();
+// Calling 'message'
+// message(); // message is not a function // this expression is not callable.
+function fn(x) {
+    function flip(x) {
+        console.log(x);
+    }
+    return x.flip(20); // x.flip is not a function
+}
+fn(4);
---
+++ Practice/index.ts
@@ -0,0 +1,15 @@
+const message = "Hello World!";
+
+// Accessing the property 'toLowerCase'
+// on 'message' and then calling it
+
+message.toLowerCase();
+
+// Calling 'message'
+// message(); // message is not a function // this expression is not callable.
+
+function fn(x) {
+  return x.flip(20); // x.flip is not a function
+}
+
+fn(4);
