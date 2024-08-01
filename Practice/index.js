c2c8842113668a8bf35c79966f904ba3bf963d16
Bhoomi Gohil <bhoomi_gohil@hotmail.com>
2024-07-31 21:29:19 -0400

    Added new code

--- Practice/index.js
+++ Practice/index.js
@@ -4,11 +4,39 @@ const message = "Hello World!";
 // on 'message' and then calling it
 message.toLowerCase();
 // Calling 'message'
-// message(); // message is not a function // this expression is not callable.
-function fn(x) {
-    function flip(x) {
-        console.log(x);
-    }
-    return x.flip(20); // x.flip is not a function
+// message(); // message is not a function in js / this expression is not callable in ts.
+// function fn(x) {
+//   return x.flip(20); // x.flip is not a function
+// }
+// fn(4);
+const user = {
+    name: "Bhoomi",
+    age: 26,
+};
+// user.location; // returns undefined in js / Property 'location' does not exist on type '{ name: string; age: number; }' in ts.
+const announcement = "Hello World!";
+// How quickly can you spot the typos?
+announcement.toLocaleLowerCase();
+// announcement.toLocalLowerCase(); // Property 'toLocalLowerCase' does not exist on type '"Hello World!"'. Did you mean 'toLocaleLowerCase'?
+// We probably meant to write this...
+announcement.toLocaleLowerCase();
+function flipCoin() {
+    // Meant to be Math.random()
+    // return Math.random < 0.5; // Operator '<' cannot be applied to types '() => number' and 'number'.
 }
-fn(4);
+const value = Math.random() < 0.5 ? "a" : "b";
+if (value !== "a") {
+    //..
+}
+// else if (value === "b") {
+//   // This comparison appears to be unintentional because the types '"a"' and '"b"' have no overlap.
+//   // Oops, unreachable
+// }
+// Greets the world.
+console.log("Hello world!");
+// This is an industrial-grade general-purpose greeter function:
+// function greet(person, date) {
+//   // Parameter 'person' & 'date' implicitly has an 'any' type.
+//   console.log(`Hello ${person}, today is ${date}`);
+// }
+// greet("Brendan"); // Expected 2 arguments, but got 1.
--- Practice/index.ts
+++ Practice/index.ts
@@ -6,10 +6,50 @@ const message = "Hello World!";
 message.toLowerCase();
 
 // Calling 'message'
-// message(); // message is not a function // this expression is not callable.
+// message(); // message is not a function in js / this expression is not callable in ts.
 
-function fn(x) {
-  return x.flip(20); // x.flip is not a function
+// function fn(x) {
+//   return x.flip(20); // x.flip is not a function
+// }
+
+// fn(4);
+
+const user = {
+  name: "Bhoomi",
+  age: 26,
+};
+
+// user.location; // returns undefined in js / Property 'location' does not exist on type '{ name: string; age: number; }' in ts.
+
+const announcement = "Hello World!";
+
+// How quickly can you spot the typos?
+announcement.toLocaleLowerCase();
+// announcement.toLocalLowerCase(); // Property 'toLocalLowerCase' does not exist on type '"Hello World!"'. Did you mean 'toLocaleLowerCase'?
+
+// We probably meant to write this...
+announcement.toLocaleLowerCase();
+
+function flipCoin() {
+  // Meant to be Math.random()
+  // return Math.random < 0.5; // Operator '<' cannot be applied to types '() => number' and 'number'.
 }
 
-fn(4);
+const value = Math.random() < 0.5 ? "a" : "b";
+if (value !== "a") {
+  //..
+}
+// else if (value === "b") {
+//   // This comparison appears to be unintentional because the types '"a"' and '"b"' have no overlap.
+//   // Oops, unreachable
+// }
+
+// Greets the world.
+console.log("Hello world!");
+
+// This is an industrial-grade general-purpose greeter function:
+function greet(person: string, date: Date) {
+  console.log(`Hello ${person}, today is ${date}`);
+}
+
+greet("Brendan", Date()); // Expected 2 arguments, but got 1.
